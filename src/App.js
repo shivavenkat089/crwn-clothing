a6d5e2d3f9482ca399d1eb695a9acc56e0e6532e
Venkat Shiva <venkata.shiva@active.ai>
2021-01-01 11:48:10 +0530

    adding header component and links

--- src/App.js
+++ src/App.js
@@ -3,23 +3,15 @@ import { Route, Switch, Redirect } from "react-router-dom";
 import "./App.css";
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
+import Header from "./components/header/header.component";
 
-function Hats(props) {
-  const { product } = props.match.params;
-  return (
-    <div>
-      Shop {product}
-      <button onClick={() => props.history.goBack()}>Go Back</button>
-    </div>
-  );
-}
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route exact path="/shop" component={ShopPage} />
-        <Route exact path="/shop/:product" component={Hats} />
         <Route component={() => <Redirect to="/" />} />
       </Switch>
     </div>
