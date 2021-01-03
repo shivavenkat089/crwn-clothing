2c7dc358f6829e8cbd218c46dc98486a30bf908c
Venkat Shiva <venkata.shiva@active.ai>
2021-01-03 10:49:11 +0530

    finishing handle submit method, so signin/up completed except error showing

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -2,7 +2,7 @@ import { Component } from 'react';
 import './sign-in.styles.scss';
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
-import { signInWithGoogle } from '../../firebase/firebase.utils';
+import { auth, signInWithGoogle } from '../../firebase/firebase.utils';
 
 class SignIn extends Component {
   constructor(props) {
@@ -18,12 +18,18 @@ class SignIn extends Component {
       [name]: value,
     });
   };
-  handleSubmit = (event) => {
+  handleSubmit = async (event) => {
     event.preventDefault();
-    this.setState({
-      email: '',
-      password: '',
-    });
+    const { email, password } = this.state;
+    try {
+      await auth.signInWithEmailAndPassword(email, password);
+      this.setState({
+        email: '',
+        password: '',
+      });
+    } catch (err) {
+      console.log('Error while login with username and password', err);
+    }
   };
   render() {
     return (
