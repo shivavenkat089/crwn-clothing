ee8406c700a1f5497a28e65926591c0368a0718f
Venkat Shiva <venkata.shiva@active.ai>
2021-01-13 21:10:32 +0530

    added addItem and removeItem functionality to checkout item component

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -1,15 +1,40 @@
+import { connect } from 'react-redux';
+import {
+  clearItemFromCartAction,
+  addItemAction,
+  removeItemAction,
+} from '../../redux/cart/cart.actions';
+
 import './checkout-item.styles.scss';
 
-const CheckoutItem = ({ cartItem: { imageUrl, name, quantity, price } }) => (
-  <div className="checkout-item">
-    <div className="image-container">
-      <img src={imageUrl} alt="checkout item" />
+const CheckoutItem = ({ cartItem, clearItem, addItem, removeItem }) => {
+  const { imageUrl, name, quantity, price } = cartItem;
+  return (
+    <div className="checkout-item">
+      <div className="image-container">
+        <img src={imageUrl} alt="checkout item" />
+      </div>
+      <span className="name">{name}</span>
+      <span className="quantity">
+        <div className="arrow" onClick={() => removeItem(cartItem)}>
+          &#10094;
+        </div>
+        <span className="value">{quantity}</span>
+        <div className="arrow" onClick={() => addItem(cartItem)}>
+          &#10095;
+        </div>
+      </span>
+      <span className="price">{price}</span>
+      <div className="remove-button" onClick={() => clearItem(cartItem)}>
+        &#10005;
+      </div>
     </div>
-    <span className="name">{name}</span>
-    <span className="quantity">{quantity}</span>
-    <span className="price">{price}</span>
-    <div className="remove-button">&#10005;</div>
-  </div>
-);
+  );
+};
 
-export default CheckoutItem;
+const dispatchActionToProps = (dispatch) => ({
+  clearItem: (item) => dispatch(clearItemFromCartAction(item)),
+  addItem: (item) => dispatch(addItemAction(item)),
+  removeItem: (item) => dispatch(removeItemAction(item)),
+});
+export default connect(null, dispatchActionToProps)(CheckoutItem);
--- src/redux/cart/cart.actions.js
+++ src/redux/cart/cart.actions.js
@@ -8,3 +8,13 @@ export const addItemAction = (item) => ({
   type: cartActionTypes.ADD_ITEM,
   payload: item,
 });
+
+export const removeItemAction = (item) => ({
+  type: cartActionTypes.REMOVE_ITEM,
+  payload: item,
+});
+
+export const clearItemFromCartAction = (item) => ({
+  type: cartActionTypes.CLEAR_ITEM_FROM_CART,
+  payload: item,
+});
