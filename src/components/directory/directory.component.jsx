d946d689e6a1e29a55024ba6ad10d89269e0aec3
Venkat Shiva <venkata.shiva@active.ai>
2021-01-16 20:05:10 +0530

    moved our directory and shop data to its own reducer, created respected selectors and updated corresponding components with new redux flow directory/shop

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,27 +1,21 @@
-import { Component } from 'react';
-
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
 import './directory.styles.scss';
 
 import MenuItem from '../menu-item/menu-item.component';
 
-import SECTIONS_DATA from '../../data/sections.data';
+import { selectDirectorySections } from '../../redux/directory/directory.selector';
 
-class Directory extends Component {
-  constructor() {
-    super();
-    this.state = {
-      sections: SECTIONS_DATA,
-    };
-  }
-  render() {
-    return (
-      <div className="directory-menu">
-        {this.state.sections.map(({ id, ...remainngParams }) => (
-          <MenuItem key={'section' + id} {...remainngParams} />
-        ))}
-      </div>
-    );
-  }
-}
+const Directory = ({ sections }) => (
+  <div className="directory-menu">
+    {sections.map(({ id, ...remainngParams }) => (
+      <MenuItem key={'section' + id} {...remainngParams} />
+    ))}
+  </div>
+);
 
-export default Directory;
+const mapStateToProps = createStructuredSelector({
+  sections: selectDirectorySections,
+});
+
+export default connect(mapStateToProps, null)(Directory);
--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,28 +1,17 @@
-import { Component } from 'react';
+import { Route } from 'react-router-dom';
 
-import CollectionPreview from '../../components/collection-preview/collection-preview.component';
+import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
+import CollectionPage from '../collection/collection.component';
 
-import SHOP_DATA from '../../data/shop.data';
+const ShopPage = ({ match }) => (
+  <div className="shop-page">
+    <Route exact path={`${match.path}`} component={CollectionsOverview} />
+    <Route
+      exact
+      path={`${match.path}/:collectionId`}
+      component={CollectionPage}
+    />
+  </div>
+);
 
-class ShopPage extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      collections: SHOP_DATA,
-    };
-  }
-  render() {
-    const { collections } = this.state;
-    return (
-      <div className="shop-page">
-        {collections.map(({ id, ...otherCollectionProps }) => (
-          <CollectionPreview
-            key={`collection-${id}`}
-            {...otherCollectionProps}
-          />
-        ))}
-      </div>
-    );
-  }
-}
 export default ShopPage;
