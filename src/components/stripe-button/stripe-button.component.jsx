25ee5819559e3d96128303271a449f15f4509509
Venkat Shiva <venkata.shiva@active.ai>
2021-01-17 09:18:16 +0530

    integrated stripe component for dummy payments

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,27 @@
+import StripeCheckout from 'react-stripe-checkout';
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey =
+    'pk_test_51IAGVtBfCUdHqvuKK0miyIMITPGlobu4k1MvQSVY4GTlLc0clXpHQRqKRLszNPzxbVLYpM8kvvmOppaEGmZG4lYq00k09rvDko';
+  const onToken = (token) => {
+    console.log(token);
+    alert('Payment Successful');
+  };
+  return (
+    <StripeCheckout
+      label="Pay Now"
+      name="CRWN Clothing Ltd."
+      billingAddress
+      shippingAddress
+      image="https://sendeyo.com/up/d/f3eb2117da"
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel="Pay Now"
+      token={onToken}
+      stripeKey={publishableKey}
+    ></StripeCheckout>
+  );
+};
+
+export default StripeCheckoutButton;
